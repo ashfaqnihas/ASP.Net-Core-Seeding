62f4642ed3f6480ccc049fadfca07b16fcff11e2
Ashfaq Ahamed <mnashfaqahamed@gmail.com>
2024-03-06 01:52:04 +0530

    Seeding

---
+++ T_Shop.Application/Common/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T_Shop.Application.DTO;
+using T_Shop.Application.DTO.Brand;
+using T_Shop.Domain.Models;
+
+namespace T_Shop.Application.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+
+            CreateMap<Brand, CreateBrandDto>().ReverseMap();
+            CreateMap<Brand, UpdateBrandDto>().ReverseMap();
+            CreateMap<Brand, BrandDto>().ReverseMap();
+        }
+    }
+}
